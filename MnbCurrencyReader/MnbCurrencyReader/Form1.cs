62ee5b7529f5605372b3da8a42a72bf29753fea2
Gergely Vidor <gergely.vidor@stud.uni-corvinus.hu>
2021-10-29 12:35:49 +0200

    Consume

--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MnbCurrencyReader.Entities;
+using MnbCurrencyReader.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,25 @@ namespace MnbCurrencyReader
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            Consume();
+            dataGridView1.DataSource = Rates;
+        }
+        void Consume()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
         }
     }
 }
